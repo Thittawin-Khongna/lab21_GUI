bd9aceac76c805a72394b1c801e1bbc14c53ff44
Thittawin <ptkk00177@gmail.com>
2025-02-21 15:01:48 +0700

    average

--- average.cpp
+++ average.cpp
@@ -1,1 +1,39 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cout << "Please input numbers to find average." << endl;
+        return 0;
+    }
+
+    vector<double> numbers;
+    for (int i = 1; i < argc; ++i) {
+        numbers.push_back(atof(argv[i]));
+    }
+
+    double sum = 0;
+    for (double num : numbers) {
+        sum += num;
+    }
+    double average = sum / numbers.size();
+
+    cout << "---------------------------------" << endl;
+    cout << "Average of " << numbers.size() << " numbers = ";
+    
+    if (floor(average) == average) {
+        cout << static_cast<int>(average); // แสดงเป็นจำนวนเต็มถ้าค่าเฉลี่ยไม่มีเศษ
+    } else {
+        cout << fixed << setprecision(3) << average;
+    }
+
+    cout << endl << "---------------------------------" << endl;
+
+    return 0;
+}
 
